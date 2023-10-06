b79da03a2a555d5e4760532351c023a853746e32
Robert den Harink <robert@robhar.com>
2023-10-06 22:01:34 +0200

    exa

--- private_dot_config/private_fish/conf.d/10_alias.fish
+++ private_dot_config/private_fish/conf.d/10_alias.fish
@@ -1,8 +1,11 @@
 alias vim="nvim"
 alias tm="tmux -2 -f ~/.config/tmux/tmux.conf new -A -s main"
+alias ls="exa"
+alias ll="exa --tree -L 2 --long --group-directories-first --icons"
 
 abbr --add gco git checkout 
 abbr --add gs git status
 abbr --add gd git diff
 abbr --add gp git push
 abbr --add gc git commit
+
