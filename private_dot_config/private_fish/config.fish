83e980d03c83a52d9ffedca9eff40fbfaa3a9ff2
Robert den Harink <robert@robhar.com>
2024-01-05 08:57:58 +0100

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,3 +10,16 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/robert/miniconda3/bin/conda
+    eval /Users/robert/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/Users/robert/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/Users/robert/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/Users/robert/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
